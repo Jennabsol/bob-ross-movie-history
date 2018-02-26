f3e9b3766841b59ce961b66ddfe0229a3df19a63
jennabsol@gmail.com <jennabsol@gmail.com>
2018-02-26 13:41:24 -0600

    added api key

--- MovieHistory/MovieHistory/wwwroot/js/site.js
+++ MovieHistory/MovieHistory/wwwroot/js/site.js
@@ -2,7 +2,7 @@
     const userSearchString = $("#movieSearch").val()
     $.ajax({
         method: "GET",
-        url: `https://api.themoviedb.org/3/search/movie?api_key=ec2f1e210a4d2150e069b9b93b4a4c12&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
+        url: `https://api.themoviedb.org/3/search/movie?api_key=ddc4c61df858b6419a822a97f67ece52&language=en-US&query=${userSearchString}&page=1&include_adult=false`,
         success: (Response => console.log(Response))
     })
 });
